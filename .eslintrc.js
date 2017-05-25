851f827267de75237f538dae41a05be30c3ef50f
ACERY1 <y841034081@gmail.com>
2017-05-25 11:20:57 +0800

    config fix

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+		"no-unused-vars": "off",
+		"no-console": "off",
+		"eqeqeq":"warn"
+    }
+};
