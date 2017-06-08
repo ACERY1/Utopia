a75ed8b8713bbf0fa05f3cc12b25c364890abf6f
ACERY1 <y841034081@gmail.com>
2017-06-08 23:37:03 +0800

    fix bug

--- src/TEST.vue
+++ src/TEST.vue
@@ -1,102 +1,34 @@
 <template>
-	<div class="header-box">
-		<el-row>
-
-			<el-menu :default-active="activeIndex" class="header" mode="horizontal" @select="handleSelect">
-				<el-col :span="display" v-for="link of links" :key="link" class="header-item">
-					<el-menu-item :index="link.link" class="header-button">{{link.name}}</el-menu-item>
-				</el-col>
-			</el-menu>
-
-		</el-row>
+	<div class="">
+		<my-header :links="links" :activeIndex="links[0].link"></my-header>
 	</div>
 </template>
 
 <script>
-	import {header} from './classes/viewModel/headerLink'
+	import myHeader from './components/bars/header.vue'
 	export default {
 		name: "",
-		components: {},
+		components: {
+			myHeader
+		},
 		data () {
 			return {
-				activeIndex: 'homepage',
-			}
-		},
-		props: {
-			links: {
-				default: new header([
+				links:[
 					{name: 'HOME', link: 'homepage'},
 					{name: 'CATEGORY', link: 'categories'},
 					{name: 'IDEAS', link: 'ideas'},
 					{name: 'CONTACT', link: 'contact'}
-				]).links
-			},
-			activeIndex: {
-				default: 'homepage'
+				]
 			}
 		},
-		computed: {
-			display () {
-				return parseInt(24 / this.links.length)
-			}
-		},
+		props:{},
+		computed: {},
 		created () {
-
 		},
-		mounted () {
-
-		},
-		methods: {
-			handleSelect(key, keyPath) {
-				console.log(key, keyPath);
-			}
-		}
+		mounted () {},
+		methods: {}
 	}
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
-	@import "common/style/mixin";
-
-	.el-menu-item.is-active {
-		color: #42b983;
-		//	@include fullBorder(1px,#42b983); button加边框
-		border-bottom: 5px solid #42b983;
-
-	}
-
-	.header-box {
-		position: fixed;
-		width: 100%;
-		box-shadow: lighten($fontClr, 40%) 1px 5px 5px;
-	}
-
-	.header {
-		height: 5rem;
-		width: 100%;
-		background: #ffffff;
-		&-item {
-			margin-top: .4rem;
-			margin-bottom: .4rem;
-			height: 4rem;
-			/*margin: 20px;*/
-		}
-		&-button {
-			@include wh(80%, 100%);
-			@include allMidBox();
-			font-size: 1.4rem;
-			font-weight: 200;
-			margin-left: 10%;
-		}
-		&-button:hover {
-			background-color: transparent;
-			border-bottom: 5px solid #42b983;
-			transition-property: border-color, background-color, color;
-			transition-duration: 0.3s, 0.3s, 0.3s;
-			transition-timing-function: initial;
-			transition-delay: initial;
-		}
-		&-buttonHover {
-			//@include fullBorder(1px,lighten($fontClr,10%))
-		}
-	}
 </style>
--- src/components/bars/header.vue
+++ src/components/bars/header.vue
@@ -1,23 +1,97 @@
 <template>
-	<div class="">
+	<div class="header-box">
+		<el-row>
 
+			<el-menu :default-active="activeIndex" class="header" mode="horizontal" @select="handleSelect">
+				<el-col :span="display" v-for="link of links" :key="link" class="header-item">
+					<el-menu-item :index="link.link" class="header-button">{{link.name}}</el-menu-item>
+				</el-col>
+			</el-menu>
+
+		</el-row>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: "",
+		name: "header",
 		components: {},
 		data () {
-			return {}
+			return {
+			}
 		},
-		computed: {},
+		props: {
+			links: {
+				default: [
+					{name: 'null', link: 'null'}
+				]
+			},
+			activeIndex: {
+				default: 'null'
+			}
+		},
+		computed: {
+			display () {
+				return parseInt(24 / this.links.length)
+			}
+		},
 		created () {
+
 		},
-		mounted () {},
-		methods: {}
+		mounted () {
+
+		},
+		methods: {
+			handleSelect(key, keyPath) {
+				console.log(key, keyPath);
+			}
+		}
 	}
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
+	@import "../../common/style/mixin";
+
+	.el-menu-item.is-active {
+		color: #42b983;
+		//	@include fullBorder(1px,#42b983); button加边框
+		border-bottom: 5px solid #42b983;
+
+	}
+
+	.header-box {
+		position: fixed;
+		width: 100%;
+		box-shadow: lighten($fontClr, 40%) 1px 5px 5px;
+	}
+
+	.header {
+		height: 5rem;
+		width: 100%;
+		background: #ffffff;
+		&-item {
+			margin-top: .4rem;
+			margin-bottom: .4rem;
+			height: 4rem;
+			/*margin: 20px;*/
+		}
+		&-button {
+			@include wh(80%, 100%);
+			@include allMidBox();
+			font-size: 1.4rem;
+			font-weight: 200;
+			margin-left: 10%;
+		}
+		&-button:hover {
+			background-color: transparent;
+			border-bottom: 5px solid #42b983;
+			transition-property: border-color, background-color, color;
+			transition-duration: 0.3s, 0.3s, 0.3s;
+			transition-timing-function: initial;
+			transition-delay: initial;
+		}
+		&-buttonHover {
+			//@include fullBorder(1px,lighten($fontClr,10%))
+		}
+	}
 </style>
